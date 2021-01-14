9a9e681d1cd418400dbf1460373b71f98516e9a5
Haosong Li <hdlee9885@hotmail.com>
2021-01-13 21:36:48 -0600

    Setting all up, added firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBAhuAXBA2NV0yW8HUnXyVeMHg7iVsXd_w",
+  authDomain: "ama-clone-9885.firebaseapp.com",
+  projectId: "ama-clone-9885",
+  storageBucket: "ama-clone-9885.appspot.com",
+  messagingSenderId: "1067270035714",
+  appId: "1:1067270035714:web:a39e69d8679f399d350a1b",
+  measurementId: "G-066Q2JQTRL"
+};
