e92f4873404bc9521572fa6793759836b94022ec
Haosong Li <hdlee9885@hotmail.com>
2021-01-14 20:52:58 -0600

    Added router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React from 'react'
 import './App.css';
 import Header from './Header'
-import Home from './Home'
+import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
-  );
+    return (
+        <Router>
+            <div className="app">
+                <Switch>
+                    <Route path="/checkout">
+                        <Header />
+                        <h1>Hi</h1>
+                    </Route>
+
+                    <Route path="/">
+                        <Header />
+                        <Home />
+                    </Route>
+
+                    
+                </Switch>
+
+            </div>
+        </Router>
+
+    );
 }
 
 export default App;
